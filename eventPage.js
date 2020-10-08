d3c89e1517dac017b413c89c6b056c0d27c7a78f
Tuukka Miettinen <tuukka1miettinen@gmail.com>
2020-10-08 18:04:37 +0300

    Restructure project to be only context menu items

--- eventPage.js
+++ eventPage.js
@@ -1,10 +1,41 @@
-// Bind tab change
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-        if (changeInfo.status === 'complete') {
-            chrome.tabs.sendMessage(tabId, {
-                message: 'TabUpdated'
-            });
-        }
-    })
+// Create contextmenu items
+var yahooMenuItem = {
+    "id": "yahoo",
+    "title": "Yahoo",
+    "contexts": ["selection"]
+};
+var marketWatchMenuItem = {
+    "id": "marketWatch",
+    "title": "MarketWatch",
+    "contexts": ["selection"]
+};
+var marketWatchChartMenuItem = {
+    "id": "marketWatchChart",
+    "title": "📈 Chart",
+    "contexts": ["selection"]
+};
+
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+}
+
+chrome.contextMenus.create(yahooMenuItem);
+chrome.contextMenus.onClicked.addListener(function(data) {
+    if (data.menuItemId == yahooMenuItem.id && data.selectionText) {
+        openInNewTab("https://finance.yahoo.com/quote/" + data.selectionText.trim())
+    }
+});
+
+chrome.contextMenus.create(marketWatchMenuItem);
+chrome.contextMenus.onClicked.addListener(function(data) {
+    if (data.menuItemId == marketWatchMenuItem.id && data.selectionText) {
+        openInNewTab("https://www.marketwatch.com/investing/stock/" + data.selectionText.trim())
+    }
+});
+chrome.contextMenus.create(marketWatchChartMenuItem);
+chrome.contextMenus.onClicked.addListener(function(data) {
+    if (data.menuItemId == marketWatchChartMenuItem.id && data.selectionText) {
+        openInNewTab("https://www.marketwatch.com/investing/stock/" + data.selectionText.trim() + "/charts")
+    }
 });
